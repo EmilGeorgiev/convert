3b08b85031ea4a49e002bfa38ede964a8fb25b17
Emil Georgiev <emogeorgiev88@gmail.com>
2023-10-30 13:57:25 +0200

    implement method convert.SrcToDst

---
+++ convert.go
@@ -0,0 +1,142 @@
+package convert
+
+import (
+	"reflect"
+	"time"
+)
+
+// SrcToDst convert src to dst. The function get values of the fields
+// of type src and assign them to the fields of type dst. The method will
+// assign values from src to dst only to this fields that have equal names.
+// The dst must be a pointer value, otherwise it wil panic.
+// For example:
+//
+//			src := struct{
+//	     	ID int
+//				Name string
+//			}{
+//	 		ID: 123,
+//				Name: "Emil",
+//			}
+//
+//			dst := struct{
+//	     	Identifier int
+//				Name string
+//			}{}
+//
+//			SrcToDst(src, &dst)
+//
+// The result will be:
+//
+//	dst = struct{
+//		ID int
+//		Name string
+//	}{0, "Emil"}
+//
+// Also the function can convert pointer fields to non pointer fields and vice versa.
+func SrcToDst(src interface{}, dst interface{}) {
+	srcValue := getNonPointerValue(reflect.ValueOf(src))
+	dstValue := getNonPointerValue(reflect.ValueOf(dst))
+	if dstValue.Kind() == reflect.Interface {
+		dstValue = dstValue.Elem()
+	}
+
+	if dstValue.Kind() == reflect.Struct {
+		setStructures(srcValue, dstValue)
+		return
+	}
+
+	setPrimitives(srcValue, dstValue)
+}
+
+func setStructures(srcValue reflect.Value, dstValue reflect.Value) {
+	nonPtrDstStruct := dstValue
+	if dstValue.Kind() == reflect.Ptr {
+		nonPtrDstStruct = dstValue.Elem()
+		if nonPtrDstStruct.Kind() == reflect.Invalid {
+			nonPtrDstStruct = reflect.New(dstValue.Type().Elem()).Elem()
+		}
+	}
+
+	for i := 0; i < srcValue.NumField(); i++ {
+		srcField := srcValue.Field(i)
+		if srcField.Kind() == reflect.Ptr && srcField.IsZero() {
+			continue
+		}
+
+		if srcField.Kind() == reflect.Slice && srcField.IsZero() {
+			continue
+		}
+
+		srcFieldName := srcValue.Type().Field(i).Name
+		dstField := nonPtrDstStruct.FieldByName(srcFieldName)
+		if !dstField.IsValid() {
+			continue
+		}
+
+		setFields(srcField, dstField)
+	}
+
+	value := nonPtrDstStruct
+	if dstValue.Kind() == reflect.Ptr {
+		value = reflect.New(value.Type())
+		value.Elem().Set(nonPtrDstStruct)
+	}
+
+	if dstValue.CanSet() {
+		dstValue.Set(value)
+	}
+
+}
+
+func setFields(srcField reflect.Value, dstField reflect.Value) {
+	nonPtrSrcField := srcField
+	if srcField.Kind() == reflect.Ptr {
+		nonPtrSrcField = srcField.Elem()
+		srcField = srcField.Elem()
+	}
+
+	nonPtrDstField := dstField.Type()
+	if dstField.Kind() == reflect.Ptr {
+		nonPtrDstField = nonPtrDstField.Elem()
+
+		srcField = reflect.New(srcField.Type())
+		srcField.Elem().Set(nonPtrSrcField)
+	}
+
+	if nonPtrSrcField.Kind() != nonPtrDstField.Kind() {
+		return
+	}
+
+	if nonPtrSrcField.Kind() == reflect.Struct && nonPtrDstField.Kind() == reflect.Struct {
+		fieldValue := nonPtrSrcField.Interface()
+		if _, ok := fieldValue.(time.Time); !ok {
+			setStructures(nonPtrSrcField, dstField)
+			return
+		}
+	}
+
+	if dstField.CanSet() {
+		dstField.Set(srcField)
+	}
+}
+
+func setPrimitives(src reflect.Value, dst reflect.Value) {
+	if !src.Type().ConvertibleTo(dst.Type()) {
+		return
+	}
+
+	src = src.Convert(dst.Type())
+	if dst.CanSet() {
+		dst.Set(src)
+	}
+}
+
+func getNonPointerValue(v reflect.Value) reflect.Value {
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+		v = getNonPointerValue(v)
+	}
+
+	return v
+}
---
+++ convert_test.go
@@ -0,0 +1,482 @@
+package convert_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/EmilGeorgiev/convert"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConvertSrcWithPtrFieldsToDstWithPtrFields(t *testing.T) {
+	id := int64(22)
+	name := "ivan"
+	isStarred := true
+	ownerType := "user"
+	userName := "Emil"
+
+	p := Template{
+		ID:         &id,
+		Name:       &name,
+		Categories: []string{"11", "22"},
+		IsStarred:  &isStarred,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  &ownerType,
+		User:       &User{FirstName: "Emil"},
+	}
+
+	id2 := int64(999)
+	got := &TemplateEntity{
+		ID: &id2,
+	}
+	convert.SrcToDst(p, &got)
+
+	idZero := int64(0)
+	want := TemplateEntity{
+		ID:         &id,
+		Name:       &name,
+		Categories: []string{"11", "22"},
+		IsStarred:  &isStarred,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  nil,
+		User:       &UserEntity{ID: &idZero, FirstName: &userName},
+	}
+
+	assert.Equal(t, want, *got)
+}
+
+func TestConvertSrcWithPtrFieldsToDstWithNonPtrFields(t *testing.T) {
+	id := int64(22)
+	name := "ivan"
+	isStarred := true
+	ownerType := int64(1)
+	userName := "Emil"
+
+	p := TemplateEntity{
+		ID:         &id,
+		Name:       &name,
+		Categories: []string{"11", "22"},
+		IsStarred:  &isStarred,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  &ownerType,
+		User:       &UserEntity{FirstName: &userName},
+	}
+
+	var got NewTemplate
+	convert.SrcToDst(p, &got)
+
+	want := NewTemplate{
+		ID:         id,
+		Name:       name,
+		Categories: []string{"11", "22"},
+		IsStarred:  isStarred,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  1,
+		User:       UserEntity{FirstName: &userName},
+	}
+
+	assert.Equal(t, want, got)
+}
+
+func TestConvertSrcWithNonPtrFieldsToDstWithPrtFields(t *testing.T) {
+	id := int64(22)
+	name := "ivan"
+	isStarred := false
+
+	p := NewTemplate{
+		ID:         id,
+		Name:       name,
+		Categories: []string{"11", "22"},
+		IsStarred:  isStarred,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  88123,
+	}
+
+	var got Template
+	convert.SrcToDst(p, &got)
+
+	want := Template{
+		ID:         &id,
+		Name:       &name,
+		Categories: []string{"11", "22"},
+		IsStarred:  &isStarred,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  nil,
+		User:       &User{FirstName: ""},
+	}
+
+	assert.Equal(t, want, got)
+}
+
+func TestConvertSrcWithNonPtrFieldsToNDstWithNonPtrFields(t *testing.T) {
+	name := "Emil"
+	p := NewTemplate{
+		ID:         int64(22),
+		Name:       "ivan",
+		Categories: []string{"11", "22"},
+		IsStarred:  true,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  88123,
+		User:       UserEntity{FirstName: &name},
+	}
+
+	var got NewTemplate
+	convert.SrcToDst(p, &got)
+
+	want := NewTemplate{
+		ID:         int64(22),
+		Name:       "ivan",
+		Categories: []string{"11", "22"},
+		IsStarred:  true,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  88123,
+		User:       UserEntity{FirstName: &name},
+	}
+
+	assert.Equal(t, want, got)
+}
+
+func TestConvertPointerSrcToPointerDst(t *testing.T) {
+	id := int64(22)
+	name := "ivan"
+	isStarred := true
+	ownerType := "user"
+
+	p := Template{
+		ID:         &id,
+		Name:       &name,
+		Categories: []string{"11", "22"},
+		IsStarred:  &isStarred,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  &ownerType,
+	}
+
+	var got TemplateEntity
+	convert.SrcToDst(&p, &got)
+
+	want := TemplateEntity{
+		ID:         &id,
+		Name:       &name,
+		Categories: []string{"11", "22"},
+		IsStarred:  &isStarred,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  nil,
+		//User: &UserEntity{},
+	}
+
+	assert.Equal(t, want, got)
+}
+
+func TestConvertSrcWithNotSetTimeFieldsToDst(t *testing.T) {
+	id := int64(22)
+	name := "ivan"
+	isStarred := true
+	ownerType := "apikey"
+
+	p := Template{
+		ID:         &id,
+		Name:       &name,
+		Categories: []string{"11", "22"},
+		IsStarred:  &isStarred,
+		OwnerType:  &ownerType,
+	}
+
+	var got TemplateEntity
+	convert.SrcToDst(p, &got)
+
+	want := TemplateEntity{
+		ID:         &id,
+		Name:       &name,
+		Categories: []string{"11", "22"},
+		IsStarred:  &isStarred,
+		OwnerType:  nil,
+		//User: &UserEntity{},
+	}
+
+	assert.Equal(t, want, got)
+	assert.Zero(t, got.CreatedAt)
+}
+
+func TestConvertSrcDstInterface(t *testing.T) {
+	id := int64(22)
+	name := "ivan"
+	isStarred := true
+	ownerType := "apikey"
+	userName := "Emil"
+	userID := int64(7878)
+
+	p := Template{
+		ID:         &id,
+		Name:       &name,
+		Categories: []string{"11", "22"},
+		IsStarred:  &isStarred,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  &ownerType,
+		User:       &User{ID: userID, FirstName: userName},
+	}
+
+	var got Printer
+	got = &TemplateEntity{}
+	convert.SrcToDst(p, got)
+
+	want := &TemplateEntity{
+		ID:         &id,
+		Name:       &name,
+		Categories: []string{"11", "22"},
+		IsStarred:  &isStarred,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  nil,
+		User:       &UserEntity{ID: &userID, FirstName: &userName},
+	}
+
+	assert.Equal(t, want, got)
+}
+
+func TestConvertSrcDstDifferentTypes(t *testing.T) {
+	u := User{
+		Permissions:       []uint{1, 2},
+		SimplePermissions: []uint{10, 11},
+	}
+
+	var got UserEntity
+	convert.SrcToDst(u, &got)
+
+	id := int64(0)
+	name := ""
+	want := UserEntity{
+		ID:                &id,
+		FirstName:         &name,
+		Permissions:       []uint{1, 2},
+		SimplePermissions: []uint{10, 11},
+	}
+
+	assert.Equal(t, want, got)
+}
+
+func TestConvertPointerZeroValueDstPtrFields(t *testing.T) {
+	name := "ivan"
+	isStarred := true
+	ownerType := "apikey"
+
+	p := Template{
+		ID:         nil,
+		Name:       &name,
+		Categories: []string{"11", "22"},
+		IsStarred:  &isStarred,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  &ownerType,
+	}
+
+	var got TemplateEntity
+	convert.SrcToDst(p, &got)
+
+	want := TemplateEntity{
+		ID:         nil,
+		Name:       &name,
+		Categories: []string{"11", "22"},
+		IsStarred:  &isStarred,
+		CreatedAt:  time.Unix(11111, 0),
+		OwnerType:  nil,
+	}
+
+	assert.Equal(t, want, got)
+}
+
+func TestConvertSrcWithNilFieldsToDstWithNotNilFields(t *testing.T) {
+	id := int64(22)
+	name := "ivan"
+	isStarred := true
+	userName := "Emil"
+
+	p := Template{
+		ID:         nil,
+		Name:       nil,
+		Categories: nil,
+		IsStarred:  nil,
+		OwnerType:  nil,
+		User:       nil,
+	}
+
+	got := TemplateEntity{
+		ID:         &id,
+		Name:       &name,
+		Categories: []string{"full", "basic"},
+		IsStarred:  &isStarred,
+		User:       &UserEntity{FirstName: &userName},
+	}
+	convert.SrcToDst(p, &got)
+
+	want := TemplateEntity{
+		ID:         &id,
+		Name:       &name,
+		Categories: []string{"full", "basic"},
+		IsStarred:  &isStarred,
+		User:       &UserEntity{FirstName: &userName},
+	}
+
+	assert.Equal(t, want, got)
+}
+
+func TestConvertSrcWithEmptyFieldSliceToDstWithNotEmptyFieldsSlice(t *testing.T) {
+	p := Template{
+		Categories: []string{},
+	}
+
+	got := TemplateEntity{
+		Categories: []string{"full", "basic"},
+	}
+	convert.SrcToDst(p, &got)
+
+	want := TemplateEntity{
+		Categories: []string{},
+	}
+
+	assert.Equal(t, want, got)
+}
+
+func TestConvertSrcWithEmptyFieldSliceToDstWithNilFieldsSlice(t *testing.T) {
+	p := Template{
+		Categories: []string{},
+	}
+
+	got := TemplateEntity{
+		Categories: nil,
+	}
+	convert.SrcToDst(p, &got)
+
+	want := TemplateEntity{
+		Categories: []string{},
+	}
+
+	assert.Equal(t, want, got)
+}
+
+func TestConvertSrcWithNilSliceToDstWithNotEmptyFieldsSlice(t *testing.T) {
+	p := Template{
+		Categories: nil,
+	}
+
+	got := TemplateEntity{
+		Categories: []string{"full", "basic"},
+	}
+	convert.SrcToDst(p, &got)
+
+	want := TemplateEntity{
+		Categories: []string{"full", "basic"},
+	}
+
+	assert.Equal(t, want, got)
+}
+
+func TestConvertAliasOfPrimitiveTypes(t *testing.T) {
+	i := MyInt(56)
+	var got MyIntEntity
+	convert.SrcToDst(i, &got)
+
+	want := MyIntEntity(56)
+	assert.Equal(t, want, got)
+}
+
+func TestConvertPrimitiveType(t *testing.T) {
+	i := 100
+	var got int64
+	convert.SrcToDst(i, &got)
+
+	assert.EqualValues(t, 100, got)
+}
+
+func TestConvertPrimitiveOfDifferentTypes(t *testing.T) {
+	i := "100"
+	var got int64
+	convert.SrcToDst(i, &got)
+	assert.EqualValues(t, 0, got)
+}
+
+func TestConvertTtToEn(t *testing.T) {
+	ui := UUID("eee")
+	b := Book{
+		ID:     UUID("ooo"),
+		UserID: &ui,
+	}
+
+	var actual BookEntity
+	convert.SrcToDst(b, &actual)
+
+	expected := BookEntity{
+		ID:     "eee",
+		UserID: "ooo",
+	}
+	assert.Equal(t, expected, actual)
+}
+
+type User struct {
+	ID                int64
+	FirstName         string
+	SimplePermissions []uint
+	Permissions       Permissions
+}
+
+type UserEntity struct {
+	ID                *int64
+	FirstName         *string
+	Permissions       Permissions
+	SimplePermissions []uint
+}
+
+type Template struct {
+	ID         *int64
+	Name       *string
+	Categories []string
+	IsStarred  *bool
+	CreatedAt  time.Time
+	OwnerType  *string
+	User       *User
+}
+
+type NewTemplate struct {
+	ID         int64
+	ExternalID string
+	Name       string
+	Categories []string
+	IsStarred  bool
+	CreatedAt  time.Time
+	OwnerType  int64
+	User       UserEntity
+}
+
+type TemplateEntity struct {
+	ID         *int64
+	Name       *string
+	Categories []string
+	IsStarred  *bool
+	CreatedAt  time.Time
+	OwnerType  *int64
+	User       *UserEntity
+}
+
+type Permissions []uint
+
+func (e TemplateEntity) Print() {
+	fmt.Println("hello")
+}
+
+type Printer interface {
+	Print()
+}
+
+type MyInt int64
+
+type MyIntEntity int64
+
+type UUID string
+
+type Book struct {
+	ID     UUID
+	UserID *UUID
+}
+
+type BookEntity struct {
+	ID     string
+	UserID string
+}
